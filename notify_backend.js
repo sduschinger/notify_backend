c39d83857fffdb2df7f3109954eeea9b3ff19bd3
duschsba <sebastian.duschinger@coi.de>
2013-04-04 23:36:54 +0200

    Switched from local storage to remote storage

--- notify_backend.js
+++ notify_backend.js
@@ -5,52 +5,72 @@ var config = require('./config'); // Local congig file to hide creds
 db = mongoose.connect(config.creds.mongoose_auth),
 Schema = mongoose.Schema; 
 
+var UserSchema = new Schema ( {
+	userid: String,
+	email: String,
+	albums: [{artist: String, albumtitle: String, available: Boolean}]
+}, {_id: false, id:false});
 
-// Create a schema for our data
-var AlbumSchema = new Schema({
-  artist: String,
-  albumtitle: String
-});
 // Use the schema to register a model
-mongoose.model('Album', AlbumSchema); 
-var Album = mongoose.model('Album');
+var User = mongoose.model('User', UserSchema);
 
 // require restify and bodyParser to read Backbone.js syncs
 var restify = require('restify');  
 var server = restify.createServer();
-server.use(restify.bodyParser());
 
 
-
-// This function is responsible for returning all entries for the Message model
-function getAlbums(req, res, next) {
-  // Resitify currently has a bug which doesn't allow you to set default headers
-  // This headers comply with CORS and allow us to server our response to any origin
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  Album.find().limit(20).execFind(function (arr,data) {
-    res.send(data);
-  });
+function getUser(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); 
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	User.find({userid:req.params.userid}).limit(20).execFind(function (arr, data) {
+		res.send(data);
+	});
 }
 
+function getUsers(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); 
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	User.find().limit(20).execFind(function (arr, data) {
+		res.send(data);
+	});
+}
 
+function saveInitial(req, res, next) {
+	var user = new User({
+		userid: req.params.userid,
+		email: "duschsba@dusch.de",
+		albums: [{artist: "artist1", albumtitle: "albumtitle1"}, {artist: "artist2", albumtitle: "albumtitle2"}]
+	});
+	user.save(function() {
+		res.send(req.body);
+	});
+}
 
-function postAlbum(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  // Create a new message model, fill it up and save it to Mongodb
-  var album = new Album();
-  console.log(req); 
-  album.artist = req.params.artist;
-  album.albumtitle = req.params.albumtitle; 
-  album.save(function () {
-    res.send(req.body);
-  });
+function saveUser(req, res, next) {
+	//console.log(req.params.userid);
+	var user = new User(req.params)
+	console.log(user);
+	//user.findOne(
+	
+	//user.save();
+	User.findOneAndUpdate({userid:req.params.userid}, { email: user.email, albums: user.albums} , {upsert: true}, function (err, doc) {});
+	res.send(req.body);
+}
+
+function remove(req, res, next) {
+	User.remove({userid:req.params.userid}, function() {res.send(req.body)});
 }
 
 // Set up our routes and start the server
-server.get('/albums', getAlbums);
-server.post('/albums', postAlbum);
+server.use(restify.bodyParser());
+server.get('/users', getUsers);
+server.get('/user/:userid', getUser);
+server.post('/user', saveUser);
+
+server.get('/initial/:userid', saveInitial);
+server.get('/remove/:userid', remove);
+
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s, love & peace', server.name, server.url);
